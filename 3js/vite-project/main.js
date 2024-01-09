177bfb39c9dc6b2ff1ed18745dc79a2bddd89e27
Manning Hitchings <mayhitchings@gmail.com>
2024-01-09 13:27:29 -0500

    added recursive infinite animation function to render and spin

--- 3js/vite-project/main.js
+++ 3js/vite-project/main.js
@@ -20,12 +20,25 @@ camera.position.setZ(30);
 
 //first arg is vectors to define
 const geometry = new THREE.TorusKnotGeometry( 10, 3, 6, 100);
-const material = new THREE.MeshToonMaterial( { color: 0x019a73, wireframe: true });
+const material = new THREE.MeshBasicMaterial( { color: 0x019a73, wireframe: true });
 const torus = new THREE.Mesh( geometry, material );
 
 scene.add(torus);
 
-const light = new THREE.AmbientLight( 0x404040 );
+// const light = new THREE.AmbientLight( 0x404040 );
 
+//recursive function (like a game loop) to create infinite loop to call render method automatically
+function animate() {
+  //tells browser want to animate something
+  requestAnimationFrame( animate );
 
-renderer.render( scene, camera );
+  //spin it
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  //whenever browser repaints screen, calls render method
+  renderer.render( scene, camera );
+}
+
+animate();
