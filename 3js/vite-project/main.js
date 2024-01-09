488a3eb469dcf6b3714715e569d28af8adf87e07
Manning Hitchings <mayhitchings@gmail.com>
2024-01-09 14:41:20 -0500

    playing with orbit controls

--- 3js/vite-project/main.js
+++ 3js/vite-project/main.js
@@ -2,6 +2,7 @@ import { TetrahedronGeometry } from 'three';
 import './style.css'
 import * as THREE from "three";
 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 
@@ -19,13 +20,31 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 camera.position.setZ(30);
 
 //first arg is vectors to define
-const geometry = new THREE.TorusKnotGeometry( 10, 3, 6, 100);
-const material = new THREE.MeshBasicMaterial( { color: 0x019a73, wireframe: true });
+const geometry = new THREE.TorusGeometry( 10, 3, 6, 100);
+const material = new THREE.MeshStandardMaterial( { color: 0xe1f07b });
 const torus = new THREE.Mesh( geometry, material );
 
 scene.add(torus);
 
-// const light = new THREE.AmbientLight( 0x404040 );
+const pointLight = new THREE.PointLight( 0xffffff );
+pointLight.position.set( 10,5,10 );
+const pointLight2 = new THREE.PointLight( 0xffffff );
+pointLight2.position.set( -5,5,10 );
+const pointLight3 = new THREE.PointLight( 0xffffff );
+pointLight3.position.set( -1,-5,15 );
+const ambientLight = new THREE.AmbientLight( 0x404040 );
+
+scene.add( pointLight, pointLight2, pointLight3, ambientLight );
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const lightHelper2 = new THREE.PointLightHelper(pointLight2);
+const lightHelper3 = new THREE.PointLightHelper(pointLight3);
+const gridHelper = new THREE.GridHelper(200,5);
+scene.add(lightHelper, lightHelper2, lightHelper3, gridHelper);
+
+//lets us move around
+const controls = new OrbitControls( camera, renderer.domElement)
+
 
 //recursive function (like a game loop) to create infinite loop to call render method automatically
 function animate() {
@@ -36,6 +55,9 @@ function animate() {
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
+
+  //mouse interaction changes reflected in ui
+  controls.update(); 
 
   //whenever browser repaints screen, calls render method
   renderer.render( scene, camera );
