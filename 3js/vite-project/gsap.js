271546a59fbc8eb7cfda6a7668f7e113bfd7e1a0
Manning Hitchings <mayhitchings@gmail.com>
2024-05-30 09:12:02 -0400

    adding gsap

--- 3js/vite-project/gsap.js
+++ 3js/vite-project/gsap.js
@@ -2,5 +2,12 @@ import { gsap } from "gsap";
     
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-
 gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".square", {
+    x: 700,
+    duration: 3,
+    scrollTrigger: ".square2"
+})
+
+
